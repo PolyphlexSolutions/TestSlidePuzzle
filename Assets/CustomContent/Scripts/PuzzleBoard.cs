29d68bcbcb8ec021118ebe4130ea77a157a7b3a8
PolyphlexSolutions <polyphlexsolutions@gmail.com>
2017-06-27 13:23:02 +0500

    Imported project

---
+++ Assets/CustomContent/Scripts/PuzzleBoard.cs
@@ -0,0 +1,201 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PuzzleBoard : MonoBehaviour
+{
+    //DOWNLOAD IMAGE
+    public Texture downloadedImg;
+    public bool isImageDownloaded = false;
+
+    
+
+    //BOARD PARAMETERS
+    public GameObject canvas;
+    public GameObject[] screens;
+    public PuzzlePiece[,] pieces;
+    public int[,] winPositions = { {0,1,2}, {3,4,5}, {6,7,8} };
+    public int piecesOnPlaceCount = 0;
+    public int boardDimension;
+    public int mainImgSquareSize;
+    public float piecesMargin;
+    
+
+    private bool isAssembled = false;
+    public bool allowInteraction = true;
+
+
+    public PuzzlePiece puzzlePiecePrefab;
+
+	// Use this for initialization
+	void Start ()
+    {
+       
+        StartCoroutine(StartWait());
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+        SetPiecesPositionalValues();
+        CheckForWin();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            PrintArrayOntoDebug();
+            Debug.Log(piecesOnPlaceCount.ToString() + "pieces");
+        }
+		
+
+	}
+
+    private void SetPiecesPositionalValues()
+    {
+
+
+
+        if (isAssembled)
+        {
+            for (int x = 0; x < boardDimension; x++)
+            {
+                for (int y = 0; y < boardDimension; y++)
+                {
+                    pieces[x, y].UpdatePositions(x, y);
+                }
+            }
+        }
+        
+    }
+    
+
+    private void CheckForWin()
+    {
+        if (isAssembled)
+        {
+            foreach (PuzzlePiece item in pieces)
+            {
+                if (!item.isOnRightPlace)
+                {
+                    break;
+                }
+                WinRoutine();
+            }
+        }
+    }
+
+    private void WinRoutine()
+    {
+        screens[0].SetActive(false);
+        screens[1].SetActive(true);
+    }
+
+    private void AssembleBoard()
+    {
+        if (isImageDownloaded)
+        {
+            pieces = new PuzzlePiece[boardDimension , boardDimension];
+            
+            float size = (mainImgSquareSize / (boardDimension * boardDimension)) * piecesMargin;
+
+            int bar = 0;
+
+            int tempX = 0, tempY = 0;
+
+            for (int y = 0; y < boardDimension; y++)
+            {
+                tempY = y;
+
+
+
+                for (int x = 0; x < boardDimension; x++)
+                {
+                    tempX = x;
+                    PuzzlePiece foo = Instantiate(puzzlePiecePrefab);
+
+                    foo.transform.parent = canvas.transform;
+
+                    foo.pawnNumber = bar;
+                    foo.gameObject.name = "Piece " + bar.ToString();
+                    bar++;
+
+                    foo.primaryX = tempX;
+                    foo.primaryY = tempY;
+
+                    
+                    foo.uiImage.texture = downloadedImg;
+                    
+
+                    pieces[tempX, tempY] = foo;
+
+                    foo = null;
+                    
+
+                }
+
+
+            }
+
+            
+
+            
+
+            System.Random randy = new System.Random();
+
+            MixArray(randy, pieces);
+        }
+        
+    }
+
+    private void MixArray(System.Random random, PuzzlePiece[,] array)
+    {
+        
+
+        for (int i = array.Length - 1; i > 0; i--)
+        {
+            int lengthRow = array.GetLength(1);
+
+            int i0 = i / lengthRow;
+            int i1 = i % lengthRow;
+
+            int j = random.Next(i + 1);
+            int j0 = j / lengthRow;
+            int j1 = j % lengthRow;
+
+            PuzzlePiece temp = array[i0, i1];
+
+            array[i0, i1] = array[j0, j1];
+            array[j0, j1] = temp;
+        }
+        isAssembled = true;
+
+    }
+
+    
+
+    private IEnumerator StartWait()
+    {
+        if (!isImageDownloaded)
+        {
+            yield return new WaitForSeconds(0.01f);
+            //Debug.Log("Wait iteration with NULL");
+            StartCoroutine(StartWait());
+        }
+        else
+        {
+            AssembleBoard();
+            yield return null;
+        }
+
+    }
+
+    private void PrintArrayOntoDebug()
+    {
+        for (int x = 3; x > 0; x--)
+        {
+            Debug.Log(pieces[0,x - 1].pawnNumber.ToString() + " " + pieces[1, x - 1].pawnNumber.ToString() + " " + pieces[2, x - 1].pawnNumber.ToString() + "\n");
+            
+        }
+    }
+}
---
+++ Assets/CustomContent/Scripts/PuzzlePiece.cs
@@ -0,0 +1,186 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PuzzlePiece : MonoBehaviour
+{
+    public RawImage uiImage;
+    public Text pawnLabel;
+    public int xPos;
+    public int yPos;
+    public int pawnNumber;
+    public float size;
+    public int primaryX = 0, primaryY = 0;
+    public bool isOnRightPlace;
+
+    private PuzzleBoard gameBoard;
+    private PuzzlePiece currentPieceFromBoard; //ссылка на элемент на доске
+
+	// Use this for initialization
+	void Start ()
+    {
+
+        gameBoard = FindObjectOfType<PuzzleBoard>();
+
+        size = (gameBoard.mainImgSquareSize / (gameBoard.boardDimension * gameBoard.boardDimension)) * gameBoard.piecesMargin;
+
+        //пячим фишку в зависимости от количества элементов
+        //uiImage.transform.localScale = new Vector3(board.piecesMargin / board.boardDimension, board.piecesMargin / board.boardDimension, board.piecesMargin / board.boardDimension);
+        uiImage.rectTransform.sizeDelta = new Vector2(size ,
+                                                      size);
+        uiImage.uvRect = new Rect(xPos * size, yPos * size, 100 / size , 100/size );
+
+        if (pawnNumber == 0)
+        {
+            uiImage.enabled = false;
+        }
+
+        foreach (PuzzlePiece item in gameBoard.pieces)
+        {
+            if (item.pawnNumber == this.pawnNumber)
+            {
+                currentPieceFromBoard = item;
+            }
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        uiImage.uvRect = new Rect(0.35f * primaryX, 0.35f * primaryY, 0.33f, 0.33f);
+
+        transform.position = transform.parent.transform.position + new Vector3(xPos * size, yPos * size, 0);
+
+
+
+        LabelHeartworkClockwork();
+
+        if (xPos == primaryX && yPos == primaryY)
+        {
+            pawnLabel.color = Color.green;
+        }
+        else
+        {
+            pawnLabel.color = Color.red;
+        }
+	}
+
+    public void UpdatePositions(int x, int y)
+    {
+        xPos = x;
+        yPos = y;
+    }
+
+    public void Clickerton()
+    {
+        if (pawnNumber != 0)
+        {
+
+            if (xPos - 1 >= 0 && gameBoard.pieces[xPos - 1, yPos].pawnNumber == 0)
+            {
+                //Debug.Log("Zero on the left " + (xPos - 1).ToString());
+                SwapTiles(xPos, yPos, xPos - 1, yPos);
+            }
+            else
+            {
+               // Debug.Log("NO Zero on the left " + (xPos - 1).ToString());
+
+
+                if (xPos + 1 <= gameBoard.boardDimension - 1 && gameBoard.pieces[xPos + 1, yPos].pawnNumber == 0)
+                {
+                    //Debug.Log("Zero on the rigth " + (xPos + 1).ToString());
+                    SwapTiles(xPos, yPos, xPos + 1, yPos);
+                }
+                else
+                {
+                    //Debug.Log("NO Zero on the right " + (xPos - 1).ToString());
+
+
+
+                    if (yPos - 1 >= 0 && gameBoard.pieces[xPos, yPos - 1].pawnNumber == 0)
+                    {
+                       // Debug.Log("Zero on the down " + (yPos - 1).ToString());
+                        SwapTiles(xPos, yPos, xPos, yPos - 1);
+                    }
+                    else
+                    {
+                        //Debug.Log("NO Zero on the down " + (yPos - 1).ToString());
+
+
+
+                        if (yPos + 1 <= gameBoard.boardDimension - 1 && gameBoard.pieces[xPos, yPos + 1].pawnNumber == 0)
+                        {
+                            //Debug.Log("Zero on the Up " + (yPos + 1).ToString());
+                            SwapTiles(xPos, yPos, xPos, yPos + 1);
+                        }
+                        else
+                        {
+                           // Debug.Log("NO Zero on the Up " + (yPos + 1).ToString());
+                        }
+
+                    }
+
+                }
+
+
+            }
+
+
+        }
+
+        
+
+
+    }
+
+    
+
+    private void SwapTiles(int oldX, int oldY, int newX, int newY)
+    {
+        //1
+        PuzzlePiece tempOld;
+        int tempNX, tempNY;
+        int tempOX, tempOY;
+
+        //2
+        tempOld = gameBoard.pieces[oldX, oldY];
+        tempOX = oldX;
+        tempOY = oldY;
+
+        tempNX = newX;
+        tempNY = newY;
+
+        //3
+        gameBoard.pieces[oldX, oldY] = gameBoard.pieces[newX, newY];
+
+        //4
+        gameBoard.pieces[newX, newY] = tempOld;
+
+        //5
+        gameBoard.pieces[oldX, oldY].xPos = tempOX;
+        gameBoard.pieces[oldX, oldY].yPos = tempOY;
+
+        gameBoard.pieces[newX, newY].xPos = tempNX;
+        gameBoard.pieces[newX, newY].yPos = tempNY;
+
+
+    }
+
+    private void LabelHeartworkClockwork()
+    {
+        if (currentPieceFromBoard != null)
+        {
+            pawnLabel.text = currentPieceFromBoard.name.ToString() + " (" + currentPieceFromBoard.xPos.ToString() + ";" + currentPieceFromBoard.yPos.ToString() + ")";
+        }
+        
+        
+            
+        
+        
+    }
+
+    
+}
